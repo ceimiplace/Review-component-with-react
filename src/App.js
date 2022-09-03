8046e13a4a753f6f5fe50490f48b2a87b38789e4
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-09-03 22:24:05 +0300

    changed to use single components for single statistic line

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ const App = (props) => {
   <Button onClick={()=>setNeutral(neutral+1)} text="medium"/>
   <Button onClick={()=>setBad(bad+1)} text="bad"/>
   <hr/> 
+  <h2>Statistics</h2>
   <Content condition1={good} condition2={bad} condition3={neutral}>
-  <Statistics good={good} bad={bad} neutral={neutral}/>
-  
+  <Statistics text="good" value={good}/>  
+  <Statistics text="bad" value={bad}/>  
+  <Statistics text="neutral" value={neutral}/>  
   <TotalReview text="The number of total reviews is " data={good+bad+neutral} fallback="No reviews at the moment"/>
   <Average data={(good - bad)/(good+bad+neutral)} text={"The average score "} fallback="No average score can be given right now" />
-  <Positive text={"The nummber of possitive review is "} data={((good*100)/(good+bad+neutral))} fallback={"Cannot show possitive reviews at the moment"}/>
+  <Positive text={"The number of possitive review is "} data={((good*100)/(good+bad+neutral))} fallback={"Cannot show possitive reviews at the moment"}/>
   </Content>
   </>
  
--- src/Statistics.js
+++ src/Statistics.js
@@ -1,8 +1,7 @@
 export default function Statistics(props){ console.log(props)
-  return <> <h2>Statistics</h2>
-  <div>good {props.good}</div>
-  <div>bad {props.bad}</div>
-  <div>neutral {props.neutral}</div>
+  return <> 
+  <div>{props.text} {props.value}</div>
+  
 
   </>
 }
