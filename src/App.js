2f1cc789360ebd7afa924fe6d236113cbf5f19f8
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-09-03 19:52:00 +0300

    Review component-React
    first project with react - a review system

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header(){
+  return <h1>Give us feedback!</h1>
+}
+function Button(props){
+  return <button onClick={props.onClick}>{props.text}</button>
+}
+function Statistics(props){ console.log(props)
+  return <> <p>Statistics</p>
+  <div>good {props.good}</div>
+  <div>bad {props.bad}</div>
+  <div>neutral {props.neutral}</div>
+
+  </>
+}
+function TotalReview({data,text,fallback}){
+  if(data === 0){
+    return <div>{fallback}</div>
+  }
+  return <div>{text}{data}</div>
+}
+function Average({data,text,fallback}){console.log(data)
+if(Number.isNaN(data)){
+  return <div>{fallback}</div>
+}
+  return <div>{text}{`${(data*100).toFixed(2)}%`}</div>
+}
+const App = (props) => {
+  const [good,setGood]=useState(0);
+  const [neutral,setNeutral]=useState(0);
+  const [bad,setBad]=useState(0);
+ 
+
+  return <> <Header/>
+  <Button onClick={()=>setGood(good+1)} text="good"/>
+  <Button onClick={()=>setNeutral(neutral+1)} text="medium"/>
+  <Button onClick={()=>setBad(bad+1)} text="bad"/>
+  <Statistics good={good} bad={bad} neutral={neutral}/>
+  <TotalReview text="The number of total reviews is " data={good+bad+neutral} fallback="No reviews at the moment"/>
+  <Average data={(good - bad)/(good+bad+neutral)} text={"The average score "} fallback="No average score can be given right now" />
+  
+  
+  </>
+ 
 }
 
-export default App;
+export default App
+
+
